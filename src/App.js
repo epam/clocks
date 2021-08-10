a4f27d7c843964da3d7976392ee7432bef0d39cd
Juratbek Makhammadaliev <Juratbek_Makhammadaliev@epam.com>
2021-08-10 15:14:22 +0500

    minimization styles and adding material ui icons, Navbar component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import './assets/styles/index.scss';
+import { Dashboard } from './pages/dashboard/Dashboard';
 
 function App() {
-    return <div className="App">React</div>;
+    return (
+        <div className="App">
+            <Dashboard />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/pages/dashboard/Dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Navbar } from '../../components/navbar/Navbar';
+
+export const Dashboard = () => {
+    return (
+        <div>
+            <Navbar />
+        </div>
+    );
+};
