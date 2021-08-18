7db8143fdba758c5bee8db063ec0ca18bb2580db
Juratbek Makhammadaliev <Juratbek_Makhammadaliev@epam.com>
2021-08-18 16:38:54 +0500

    methods for working with url

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { LocationsProvider } from './context/locations';
 import Dashboard from './pages/dashboard';
 import './assets/styles/index.scss';
 
 function App() {
-    useEffect(() => {
-        // history.push('dashboard');
-    }, []);
-
     return (
         <LocationsProvider>
             <div className="App">
--- src/context/locations/hook.js
+++ src/context/locations/hook.js
@@ -1,57 +1,60 @@
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { getCurrentUserLocation, convertArray } from '../../handlers';
-import { useUrlParams } from '../../hooks/useUrlParams/useUrlParams';
+import { useQueryParams } from '../../hooks/useUrlParams/useQueryParams';
+
+const paramKeyWord = 'locations';
 
 export const useLocations = () => {
     const [locations, setLocations] = useState([]);
-    console.log('🚀 ~ file: hook.js ~ line 8 ~ useLocations ~ locations', locations);
     const [hasCreateForm, setHasCreateForm] = useState(false);
 
     const location = useLocation();
 
-    const { SetParam, GetParam } = useUrlParams();
-
-    const GetLocationsFromParams = () => {
-        return GetParam('locations') || [];
-    };
+    const { SetParam, GetParam } = useQueryParams();
 
     const CreateFormHandler = hasForm => {
         if (hasForm && typeof hasForm === 'boolean') {
             return setHasCreateForm(hasForm);
         }
-        setHasCreateForm(!hasCreateForm);
+        setHasCreateForm(prev => !prev);
     };
 
     useEffect(() => {
-        const locations = GetLocationsFromParams();
+        const locations = GetParam(paramKeyWord) || [];
         if (!locations || !locations.length) {
             const { city, country, timezone } = getCurrentUserLocation();
-            const locations = [
-                {
-                    city,
-                    country,
-                    timezone
-                }
-            ];
-            SetParam('locations', JSON.stringify(locations));
+            const locations = [{ city, country, timezone, message: '' }];
+            return SetParam(paramKeyWord, JSON.stringify(locations));
         }
+        if (!Array.isArray(locations)) {
+            return console.error('Locations are not valid');
+        }
+        SetParam(paramKeyWord, JSON.stringify(convertArray(locations)));
     }, []);
 
     useEffect(() => {
-        const locationsFromUlrParams = GetLocationsFromParams();
+        const locationsFromUlrParams = GetParam(paramKeyWord) || [];
+        if (!Array.isArray(locationsFromUlrParams)) {
+            return console.error('Locations form url must be array');
+        }
         const convertedLocations = convertArray(locationsFromUlrParams);
         setLocations(convertedLocations);
     }, [location.search]);
 
     const AddLocation = ({ city, country, message = '', timezone }) => {
-        const locationsFromUrl = GetLocationsFromParams();
-        locationsFromUrl.push({ city, country, message, timezone });
-        SetParam('locations', locationsFromUrl);
+        const locationsFromUrl = GetParam(paramKeyWord) || [];
+        locationsFromUrl.push({
+            city,
+            country,
+            message,
+            timezone
+        });
+        SetParam(paramKeyWord, locationsFromUrl);
     };
 
-    const DeleteLocation = location => {
-        console.log(location);
+    const DeleteLocation = () => {
+        // delete logic here
     };
 
     return {
