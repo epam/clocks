2c660aed042abcbb0acf0e048337d458da664e4a
Juratbek Makhammadaliev <Juratbek_Makhammadaliev@epam.com>
2021-08-26 10:00:12 +0500

    checking for undefined values

--- src/handlers/generateIdFormat.js
+++ src/handlers/generateIdFormat.js
@@ -1,6 +1,6 @@
 const generateIdFormat = (cityAscii, iso2, lat, lng) => {
     if (!(cityAscii && iso2 && lat && lng)) {
-        console.error('Id for adding location is not valid!');
+        console.error('Fields for generating id are not valid!');
     }
     return `${cityAscii}_${iso2}_${parseInt(Math.abs(lat), 10)}_${parseInt(Math.abs(lng), 10)}`;
 };
