adc3e0559cb39e8445631e5c5ba1ea8d6ed8f509
nizom <nizomabdumalikov@gmail.com>
2021-09-09 02:38:06 +0500

    Created add comment ui

--- src/containers/location/Location.js
+++ src/containers/location/Location.js
@@ -1,24 +1,19 @@
 import React, { useContext } from 'react';
 
 import { IconButton, Typography } from '@material-ui/core';
-import moment from 'moment-timezone';
 import { Gear } from '../../assets/icons/icons';
 import { LocationsContext } from '../../context/locations';
 import LocationDropdown from '../../components/locationDropdown';
 import LocationOffsets from '../../components/locationOffsets';
+import LocationContent from '../../components/locationContent';
 import css from './Location.module.scss';
 
-const Location = ({ timezone, city, country, offset, host, message, id }) => {
+const Location = ({ timezone, city, country, offset, host, id, message }) => {
     const { hours, minutes } = offset;
-    const [currentTime, setCurrentTime] = React.useState(moment.tz(timezone));
-    const [drawerVisibility, setDrawerVisibility] = React.useState(false);
     const { actions } = useContext(LocationsContext);
 
-    React.useEffect(() => {
-        const id = setInterval(() => setCurrentTime(moment.tz(timezone)), 1000);
-
-        return () => clearInterval(id);
-    }, [timezone]);
+    const [drawerVisibility, setDrawerVisibility] = React.useState(false);
+    const [messageVisibility, setMessageVisibility] = React.useState(false);
 
     const handleDelete = () => {
         actions.DeleteLocation(id);
@@ -37,28 +32,17 @@ const Location = ({ timezone, city, country, offset, host, message, id }) => {
             <LocationDropdown
                 visibility={drawerVisibility}
                 setVisibility={value => setDrawerVisibility(value)}
+                addComment={() => setMessageVisibility(true)}
                 deleteCity={handleDelete}
             />
             <LocationOffsets hours={hours} minutes={minutes} host={host} />
 
-            <span className={css.time}>
-                <Typography variant="h2" className={css.hour}>
-                    {currentTime.format('HH')}
-                </Typography>
-                <Typography variant="h2">{currentTime.format('mm')}</Typography>
-            </span>
-
-            <Typography paragraph variant="subtitle2" className={css.grey}>
-                {currentTime.format('D MMM')}
-            </Typography>
-            <Typography variant="h5">{city}</Typography>
-            <Typography variant="subtitle2" className={css.country}>
-                {country}
-            </Typography>
-            <Typography variant="subtitle2" className={css.grey}>
-                {timezone} GMT {moment.tz(moment.utc(), timezone).utcOffset() / 60}
-            </Typography>
-            <Typography variant="body2">{message}</Typography>
+            <LocationContent city={city} country={country} timezone={timezone} />
+            {messageVisibility ? (
+                <textarea name="" id="" cols="20" rows="3" />
+            ) : (
+                <Typography variant="subtitle2">{message}</Typography>
+            )}
         </div>
     );
 };
