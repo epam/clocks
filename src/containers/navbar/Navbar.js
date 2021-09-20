6ff7a69501113a8e3d1bd8d2a74d19036e06bfd2
Juratbek Makhammadaliev <juratbektfi1998@gmail.com>
2021-09-20 14:54:18 +0500

    drawing planning mode scrollbar and switch

--- src/containers/navbar/Navbar.js
+++ src/containers/navbar/Navbar.js
@@ -3,6 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import { AppBar, Toolbar, Button } from '@material-ui/core';
 import { LocationsContext } from '../../context/locations';
 import logo from '../../assets/images/logo.svg';
+import { PlanningMode } from '../../components/planningMode/PlanningMode';
 
 const useStyles = makeStyles(theme => ({
     appbar: {
@@ -13,7 +14,9 @@ const useStyles = makeStyles(theme => ({
     },
     toolbar: {
         display: 'flex',
-        justifyContent: 'space-between'
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        height: '88px'
     },
     menuButton: {
         marginRight: theme.spacing(2)
@@ -36,6 +39,7 @@ const Navbar = () => {
                 <Button className={classes.title} onClick={actions.ResetUrl}>
                     <img src={logo} alt="logo" />
                 </Button>
+                <PlanningMode />
                 <Button variant="outlined" color="inherit" onClick={() => actions.CreateFormHandler(true)}>
                     Add City
                 </Button>
