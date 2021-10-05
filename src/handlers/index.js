59dea42d5dce8acdbf9ad093c3aa7e477eb4886f
nizom <nizomabdumalikov@gmail.com>
2021-10-05 15:42:35 +0500

    Created GetUserLocation function

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -1,7 +1,6 @@
-import convertArray from './convertArray';
 import convertData from './convertData';
 import getCurrentUserLocation from './getCurrentUserLocation';
 import generateIdFormat from './generateIdFormat';
 import convertToArrayWithIdField from './convertToArrayWithIdField';
 
-export { convertArray, convertData, getCurrentUserLocation, generateIdFormat, convertToArrayWithIdField };
+export { convertData, getCurrentUserLocation, generateIdFormat, convertToArrayWithIdField };
