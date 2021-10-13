dc48a1176dfa627481b4507109aca351edbf55b2
Juratbek Makhammadaliev <juratbektfi1998@gmail.com>
2021-10-13 14:56:28 +0500

    generateIdFormat function test

--- src/handlers/generateIdFormat.js
+++ src/handlers/generateIdFormat.js
@@ -1,6 +1,14 @@
 const generateIdFormat = (cityAscii, iso2, lat, lng) => {
     if (!(cityAscii && iso2 && lat && lng)) {
-        console.error('Fields for generating id are not valid!');
+        return '';
+    }
+    if (
+        typeof cityAscii !== 'string' ||
+        typeof iso2 !== 'string' ||
+        Number.isNaN(parseInt(lat, 10)) ||
+        Number.isNaN(parseInt(lng, 10))
+    ) {
+        return '';
     }
     return `${cityAscii}_${iso2}_${parseInt(Math.abs(lat), 10)}_${parseInt(Math.abs(lng), 10)}`;
 };
