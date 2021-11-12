cbade6907a41a2e940c304fab025268c49c2a623
nizom <nizomabdumalikov@gmail.com>
2021-11-12 16:44:36 +0500

    Fixed EPAM logo size on navbar

--- src/containers/Navbar/Navbar.tsx
+++ src/containers/Navbar/Navbar.tsx
@@ -26,7 +26,8 @@ const useStyles = makeStyles(theme => ({
     title: {
         height: 28,
         objectFit: 'cover',
-        cursor: 'pointer'
+        cursor: 'pointer',
+        marginRight: 12
     },
     buttons: {
         display: 'flex',
@@ -60,11 +61,9 @@ const Navbar = () => {
     return (
         <AppBar position="static" className={classes.appbar} color="transparent">
             <Toolbar className={classes.toolbar}>
-                {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-noninteractive-element-interactions */}
                 <div className="content-center">
-                    <Button className={classes.title} onClick={ResetUrl}>
-                        <img src={logo} alt="logo" />
-                    </Button>
+                    {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-noninteractive-element-interactions */}
+                    <img className={classes.title} onClick={ResetUrl} src={logo} alt="logo" />
                     <DashboardName />
                 </div>
                 <div className={classes.buttons}>
