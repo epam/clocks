b23dfa107adadd79832e9496588a249d2f3337c3
Jur'atbek <juratbektfi1998@gmail.com>
2021-11-16 18:01:04 +0500

    test cases for dark mode

--- src/assets/icons/icons.js
+++ src/assets/icons/icons.js
@@ -83,7 +83,7 @@ const Comment = ({ color }) => (
 );
 
 const DarkModeIcon = () => (
-    <svg width="32px" viewBox="0 0 24 24" data-testid="DarkModeIcon" aria-hidden="true">
+    <svg width="32px" data-test-id="DarkModeIcon" viewBox="0 0 24 24" data-testid="DarkModeIcon" aria-hidden="true">
         <path
             fill="#FFF"
             d="M20 8.69V4h-4.69L12 .69 8.69 4H4v4.69L.69 12 4 15.31V20h4.69L12 23.31 15.31 20H20v-4.69L23.31 12 20 8.69zM12 18c-.89 0-1.74-.2-2.5-.55C11.56 16.5 13 14.42 13 12s-1.44-4.5-3.5-5.45C10.26 6.2 11.11 6 12 6c3.31 0 6 2.69 6 6s-2.69 6-6 6z"
@@ -92,7 +92,7 @@ const DarkModeIcon = () => (
 );
 
 const LightModeIcon = () => (
-    <svg width="32px" viewBox="0 0 24 24" data-testid="LightModeIcon" aria-hidden="true">
+    <svg width="32px" data-test-id="LightModeIcon" viewBox="0 0 24 24" data-testid="LightModeIcon" aria-hidden="true">
         <path
             fill="#FFF"
             d="M20 8.69V4h-4.69L12 .69 8.69 4H4v4.69L.69 12 4 15.31V20h4.69L12 23.31 15.31 20H20v-4.69L23.31 12 20 8.69zM12 18c-3.31 0-6-2.69-6-6s2.69-6 6-6 6 2.69 6 6-2.69 6-6 6zm0-10c-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4z"
