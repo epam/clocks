07912c3a204875a25d526bf8497427be0c9b306d
Jur'atbek <juratbektfi1998@gmail.com>
2021-11-16 18:01:04 +0500

    test cases for dark mode

--- src/containers/Navbar/Navbar.test.js
+++ src/containers/Navbar/Navbar.test.js
@@ -1,10 +1,13 @@
+import React from 'react';
 import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { LocationsContext } from '../../context/locations';
+import { ThemeContext, useTheme } from '../../context/theme';
 import Navbar from './Navbar';
 
 const MockResetUrl = jest.fn();
 const MockCreateFormHandler = jest.fn();
+const MockThemeHandler = jest.fn();
 
 const wrapper = children => (
     <LocationsContext.Provider
@@ -13,6 +16,21 @@ const wrapper = children => (
         {children}
     </LocationsContext.Provider>
 );
+
+const mockThemeWrapper = children => (
+    <ThemeContext.Provider value={{ state: { type: 'light' }, actions: { ThemeHandler: MockThemeHandler } }}>
+        {children}
+    </ThemeContext.Provider>
+);
+
+const ThemeWrapper = () => {
+    const store = useTheme();
+    return (
+        <ThemeContext.Provider value={store}>
+            <Navbar />
+        </ThemeContext.Provider>
+    );
+};
 
 jest.mock('../../hooks/useQueryParams', () => {
     const originalModule = jest.requireActual('../../hooks/useQueryParams');
@@ -25,15 +43,17 @@ jest.mock('../../hooks/useQueryParams', () => {
 
 describe('test Navbar component', () => {
     it('renders Navbar component', () => {
-        const { getByRole, getByText } = render(<Navbar />);
+        const { getByRole, getByText, getByTestId } = render(<Navbar />);
         const navbar = getByRole('banner');
         const logoButton = getByRole('button', { name: 'logo' });
         const logoImg = getByRole('img', { name: 'logo' });
         const addCityButton = getByText('Add City');
+        const themeIcon = getByTestId(/DarkModeIcon/i);
         expect(navbar).toBeInTheDocument();
         expect(logoButton).toBeInTheDocument();
         expect(logoImg).toBeInTheDocument();
         expect(addCityButton).toBeInTheDocument();
+        expect(themeIcon).toBeInTheDocument();
     });
     it('reset url by clocking the logo', () => {
         const { getByRole } = render(wrapper(<Navbar />));
@@ -47,5 +67,18 @@ describe('test Navbar component', () => {
         userEvent.click(addCityButton);
         expect(MockCreateFormHandler).toHaveBeenCalledTimes(1);
         expect(MockCreateFormHandler).toHaveBeenCalledWith(true);
+    });
+    it('changes theme by clicking theme icon button', () => {
+        const { getByTestId } = render(mockThemeWrapper(<Navbar />));
+        const themeIcon = getByTestId(/LightModeIcon/i);
+        userEvent.click(themeIcon);
+        expect(MockThemeHandler).toHaveBeenCalledTimes(1);
+    });
+    it('changes theme icon', () => {
+        const { getByTestId } = render(<ThemeWrapper />);
+        const lightThemeIcon = getByTestId(/LightModeIcon/i);
+        userEvent.click(lightThemeIcon);
+        const darkThemeIcon = getByTestId(/DarkModeIcon/i);
+        expect(darkThemeIcon).toBeInTheDocument();
     });
 });
