23935137b5a69ced45b4bf8b04707c75432a9d03
Oybek Mamatov <oybek_mamatov@epam.com>
2021-11-19 16:44:33 +0500

    Add CKEditor in location.tsx

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,8 +1,9 @@
 import { EpamColors } from './colors';
+import { editorConfig } from './ckeditorConfig';
 import { InitialContext } from './initialContext';
 
 const PARAM_KEYWORD = 'locations';
 const DASHBOARD_PARAM_KEYWORD = 'name';
 const CURRENT_USER_LOCATION_ID = 'CURRENT_USER_LOCATION_ID';
 
-export { PARAM_KEYWORD, EpamColors, CURRENT_USER_LOCATION_ID, InitialContext, DASHBOARD_PARAM_KEYWORD };
+export { PARAM_KEYWORD, EpamColors, editorConfig, CURRENT_USER_LOCATION_ID, InitialContext, DASHBOARD_PARAM_KEYWORD };
