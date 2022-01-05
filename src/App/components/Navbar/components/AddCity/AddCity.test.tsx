e7b4d33f97e175b8c5d1548408b0d013ddbb52a9
Jur'atbek <juratbektfi1998@gmail.com>
2022-01-05 10:51:40 +0500

    resolving tests

--- src/App/components/Navbar/components/AddCity/AddCity.test.tsx
+++ src/App/components/Navbar/components/AddCity/AddCity.test.tsx
@@ -1,50 +1,49 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-
 import { I18nextProvider } from 'react-i18next';
-import { LocationsContext } from '../../../../context/locations';
+
 import AddCity from './AddCity';
 import i18n from '../../../../dictionary';
 
 const MockAddLocation = jest.fn();
 const MockVisibilityHandler = jest.fn();
 
+jest.mock('../../../../hooks/useUrl', () => ({
+  useUrl: () => ({
+    GetLocationsFromUrl: () => [
+      'Tashkent_UZ_41_69',
+      'Seoul_KR_37_126',
+      'Tokyo_JP_35_139'
+    ],
+    AddLocation: MockAddLocation
+  })
+}));
+
 const MockComponent = () => {
-  const GetLocationsFromUrl = () => [
-    'Tashkent_UZ_41_69',
-    'Seoul_KR_37_126',
-    'Tokyo_JP_35_139'
-  ];
-  const store = {
-    actions: { GetLocationsFromUrl, AddLocation: MockAddLocation },
-    state: {}
-  };
   return (
     <I18nextProvider i18n={i18n}>
-      <LocationsContext.Provider value={store}>
-        <AddCity visibility visibilityHandler={MockVisibilityHandler} />
-      </LocationsContext.Provider>
+      <AddCity visibility visibilityHandler={MockVisibilityHandler} />
     </I18nextProvider>
   );
 };
 
-fdescribe('AddCity component', () => {
+describe('AddCity component', () => {
   it('renders properly', () => {
-    // render(<MockComponent />);
-    // expect(screen.getByLabelText('Close Drawer Button')).toBeInTheDocument();
-    // expect(screen.getByTestId(/search-input/i)).toBeInTheDocument();
-    // expect(screen.getByRole('menu')).toBeInTheDocument();
+    render(<MockComponent />);
+    expect(screen.getByLabelText('Close Drawer Button')).toBeInTheDocument();
+    expect(screen.getByTestId(/search-input/i)).toBeInTheDocument();
+    expect(screen.getByRole('menu')).toBeInTheDocument();
   });
   it('closes on button click', () => {
-    // render(<MockComponent />);
-    // userEvent.click(screen.getByLabelText('Close Drawer Button'));
-    // expect(MockVisibilityHandler).toHaveBeenCalledTimes(1);
+    render(<MockComponent />);
+    userEvent.click(screen.getByLabelText('Close Drawer Button'));
+    expect(MockVisibilityHandler).toHaveBeenCalledTimes(1);
   });
   it('input correctly works', () => {
-    // render(<MockComponent />);
-    // const input = screen.getByTestId(/search-input/i);
-    // userEvent.type(input, 'Tokyo');
-    // expect(screen.getByText('Tokyo')).toBeInTheDocument();
-    // expect(screen.getByText('Added')).toBeInTheDocument();
+    render(<MockComponent />);
+    const input = screen.getByTestId(/search-input/i);
+    userEvent.type(input, 'Tokyo');
+    expect(screen.getByText('Tokyo')).toBeInTheDocument();
+    expect(screen.getByText('Added')).toBeInTheDocument();
   });
 });
