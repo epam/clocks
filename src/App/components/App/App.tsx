6d563bf892e5285484a8ccbb0013d06d5dda78f2
Jur'atbek <juratbektfi1998@gmail.com>
2022-01-05 10:17:28 +0500

    add Footer component into app component

--- src/App/components/App/App.tsx
+++ src/App/components/App/App.tsx
@@ -21,6 +21,7 @@ function App() {
                 <Dashboard />
               </Route>
             </Switch>
+            <Footer />
           </LocationsProvider>
         </PlanningModeProvider>
       </SnackbarProvider>
