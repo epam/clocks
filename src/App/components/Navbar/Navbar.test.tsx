4672821cb1175cf70775309f3ea08c388deb9efe
Jur'atbek <juratbektfi1998@gmail.com>
2022-01-06 11:08:14 +0500

    resolve Navbar test fails

--- src/App/components/Navbar/Navbar.test.tsx
+++ src/App/components/Navbar/Navbar.test.tsx
@@ -6,29 +6,34 @@ import Navbar from './Navbar';
 import i18n from '../../dictionary';
 
 const MockAddCitySidebarHandler = jest.fn();
+const MockResetUrl = jest.fn();
 
 const wrapper = (children: any) => (
   <I18nextProvider i18n={i18n}>{children}</I18nextProvider>
 );
 
-jest.mock('../../hooks/useQueryParams', () => {
-  const originalModule = jest.requireActual('../../hooks/useQueryParams');
-  return {
-    __esModule: true,
-    ...originalModule,
-    useQueryParams: () => ({
-      SetParam: jest.fn(),
-      GetParam: jest.fn(),
-      DeleteParam: jest.fn()
-    })
-  };
-});
+jest.mock('../../hooks/useQueryParams', () => ({
+  useQueryParams: () => ({
+    SetParam: jest.fn(),
+    GetParam: jest.fn(),
+    DeleteParam: jest.fn()
+  })
+}));
+
+jest.mock('../../hooks/useUrl', () => ({
+  useUrl: () => ({
+    ResetUrl: MockResetUrl
+  })
+}));
 
 describe('test Navbar component', () => {
   it('renders Navbar component', () => {
     const { getByRole, getByText, getByTestId } = render(
       <I18nextProvider i18n={i18n}>
-        <Navbar addCitySidebarHandler={MockAddCitySidebarHandler} />
+        <Navbar
+          locations={[]}
+          addCitySidebarHandler={MockAddCitySidebarHandler}
+        />
       </I18nextProvider>
     );
     const navbar = getByRole('banner');
@@ -44,15 +49,25 @@ describe('test Navbar component', () => {
   });
   it('reset url by clocking the logo', () => {
     const { getByRole } = render(
-      wrapper(<Navbar addCitySidebarHandler={MockAddCitySidebarHandler} />)
+      wrapper(
+        <Navbar
+          locations={[]}
+          addCitySidebarHandler={MockAddCitySidebarHandler}
+        />
+      )
     );
     const logoButton = getByRole('button', { name: 'logo' });
     userEvent.click(logoButton);
-    // expect(MockResetUrl).toHaveBeenCalledTimes(1);
+    expect(MockResetUrl).toHaveBeenCalledTimes(1);
   });
   it('open sidebar by clicking the Add City Button', () => {
     const { getByText } = render(
-      wrapper(<Navbar addCitySidebarHandler={MockAddCitySidebarHandler} />)
+      wrapper(
+        <Navbar
+          locations={[]}
+          addCitySidebarHandler={MockAddCitySidebarHandler}
+        />
+      )
     );
     const addCityButton = getByText('ADD CITY');
     userEvent.click(addCityButton);
