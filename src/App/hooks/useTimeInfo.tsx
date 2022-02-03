2bdcc4463b5a465d8b04d7ca68404993568462e0
Oybek Mamatov <oybek_mamatov@epam.com>
2022-02-03 12:33:12 +0500

    Add logic for planning mode

--- src/App/hooks/useTimeInfo.tsx
+++ src/App/hooks/useTimeInfo.tsx
@@ -7,7 +7,9 @@ import { ILocation, IInitialState } from '../redux/types';
 const useTimeInfo = (location?: ILocation) => {
   const { t } = useTranslation();
 
-  const { userLocation, timeFormat } = useSelector((state: IInitialState) => state);
+  const { userLocation, timeFormat, additionalHours, planningMode } = useSelector(
+    (state: IInitialState) => state
+  );
 
   const date = new Date();
 
@@ -27,9 +29,86 @@ const useTimeInfo = (location?: ILocation) => {
     })
     .split(':');
 
-  timeObject.hours = visiableTime[0];
-  timeObject.minutes = visiableTime[1];
-  timeObject.suffix = visiableTime[2].substring(3);
+  // Adding additional hours
+  const tempDate = {
+    hours: parseInt(visiableTime[0]),
+    minutes: parseInt(visiableTime[1]),
+    suffix: visiableTime[2].substring(3),
+    day: date.getDay()
+  };
+
+  if (planningMode) {
+    const integerAdditionalHours =
+      additionalHours < 0 ? Math.ceil(additionalHours) : Math.floor(additionalHours);
+    const additionalMinutes = 60 * (additionalHours - integerAdditionalHours);
+    const tempMinutes = tempDate.minutes + additionalMinutes;
+    const hoursBefore = tempDate.hours + integerAdditionalHours;
+
+    // Setting the right minutes
+    if (tempMinutes > 60) {
+      tempDate.hours += 1;
+      tempDate.minutes = tempMinutes - 60;
+    } else if (tempMinutes === 60) {
+      tempDate.hours += 1;
+      tempDate.minutes = 0;
+    } else if (tempMinutes < 0) {
+      if (tempDate.hours === 0) {
+        tempDate.hours = 23;
+      } else {
+        tempDate.hours -= 1;
+      }
+      tempDate.minutes = 60 + tempMinutes;
+    } else {
+      tempDate.minutes = tempMinutes;
+    }
+
+    // Settings the right hours
+    if (isHour12Format) {
+      if (tempDate.hours + integerAdditionalHours > 12) {
+        tempDate.hours = tempDate.hours + integerAdditionalHours - 12;
+      } else if (tempDate.hours + integerAdditionalHours === 12) {
+        tempDate.hours = 12;
+      } else if (tempDate.hours + integerAdditionalHours < 0) {
+        tempDate.hours = 12 + tempDate.hours + integerAdditionalHours;
+      } else if (tempDate.hours + integerAdditionalHours === 0) {
+        tempDate.hours = 12;
+      } else {
+        tempDate.hours = tempDate.hours + integerAdditionalHours;
+      }
+      console.log('after', hoursBefore, tempDate.hours, integerAdditionalHours);
+
+      // Setting right AM/PM
+      if (hoursBefore !== tempDate.hours) {
+        if ((hoursBefore === 12 || hoursBefore === 0) && tempDate.hours === 11) {
+          tempDate.suffix = tempDate.suffix === 'AM' ? 'PM' : 'AM';
+        } else if (hoursBefore === 11 && (tempDate.hours === 12 || tempDate.hours === 0)) {
+          tempDate.suffix = tempDate.suffix === 'AM' ? 'PM' : 'AM';
+        }
+      }
+    } else {
+      if (tempDate.hours + integerAdditionalHours > 24) {
+        tempDate.hours = tempDate.hours + integerAdditionalHours - 24;
+      } else if (tempDate.hours + integerAdditionalHours === 24) {
+        tempDate.hours = 0;
+      } else if (tempDate.hours + integerAdditionalHours < 0) {
+        tempDate.hours = 24 + tempDate.hours + integerAdditionalHours;
+      } else {
+        tempDate.hours = tempDate.hours + integerAdditionalHours;
+      }
+    }
+  }
+
+  timeObject.hours = tempDate.hours < 10 ? `0${tempDate.hours}` : `${tempDate.hours}`;
+  timeObject.minutes = tempDate.minutes < 10 ? `0${tempDate.minutes}` : `${tempDate.minutes}`;
+  timeObject.suffix = tempDate.suffix;
+
+  const newDate = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    tempDate.day,
+    tempDate.hours,
+    tempDate.minutes
+  );
 
   if (userLocation) {
     const locationTime = date
