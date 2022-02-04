ceab581a993f6de8529faa0687aa9c880a1b9aee
Juratbek <juratbektfi1998@gmail.com>
2022-02-04 17:17:19 +0500

    adding DragDropMode component

---
+++ src/App/components/Header/components/DragDropMode/DragDropMode.tsx
@@ -0,0 +1,33 @@
+import { IconButton } from '@mui/material';
+import { LibraryBooksOutlined } from '@mui/icons-material';
+import clsx from 'clsx';
+import { useDispatch, useSelector } from 'react-redux';
+
+import useTheme from '../../../../hooks/useTheme';
+import { IInitialState } from '../../../../redux/types';
+
+import style from './DragDropMode.module.scss';
+import { setDragDropMode } from '../../../../redux/actions';
+
+const DragDropMode = () => {
+  const iconTheme = useTheme(style.lightIcon, style.darkIcon);
+  
+  const { deleteMode } = useSelector((state: IInitialState) => state);
+  const { dragDropMode } = useSelector((state: IInitialState) => state);
+
+  const dispatch = useDispatch();
+
+  const handleSetDragDropMode = () => {
+    dispatch(setDragDropMode(!dragDropMode));
+  };
+
+  return (
+    <IconButton onClick={handleSetDragDropMode}>
+      <LibraryBooksOutlined
+        className={clsx({ [iconTheme]: true, [style.disabledIcon]: deleteMode })}
+      />
+    </IconButton>
+  );
+};
+
+export default DragDropMode;
