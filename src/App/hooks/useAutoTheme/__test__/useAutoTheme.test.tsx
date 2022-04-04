2967cd887eb8bbac1b5e24237696a0d69a9d3ff8
Jamshid Tursunov <istudyatinhauniversity@gmail.com>
2022-04-05 00:47:41 +0500

    feat: 279. Unit test for useAutoTheme hook

--- src/App/hooks/useAutoTheme/__test__/useAutoTheme.test.tsx
+++ src/App/hooks/useAutoTheme/__test__/useAutoTheme.test.tsx
@@ -1,14 +1,107 @@
-import React, { FC } from 'react';
-import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { renderHook, act } from '@testing-library/react-hooks';
+
 import useAutoTheme from '../useAutoTheme';
+import reducer from '../../../redux/reducer';
+import { THEME } from '../../../redux/constants';
 
-test('useAutoTheme hook', () => {
-  function Wrapper() {
-    const { setAutoTheme } = useAutoTheme();
+jest.mock('react-i18next', () => ({
+  useTranslation: () => jest.fn()
+}));
 
-    return null;
+let localStorageMock = (function () {
+  var store: any = {};
+  return {
+    getItem(key: string) {
+      return store[key];
+    },
+    setItem(key: string, value: string) {
+      store[key] = value.toString();
+    },
+    clear() {
+      store = {};
+    },
+    removeItem(key: string) {
+      delete store[key];
+    }
+  };
+})();
+
+const MockMatchMedia = (query: string) => {
+  switch (query) {
+    case '(prefers-color-scheme)':
+      return {
+        matches: true,
+        media: query
+      };
+    case `(prefers-color-scheme: ${THEME.dark})`:
+      return {
+        matches: true,
+        media: query
+      };
+    default:
+      return {
+        matches: false,
+        media: query
+      };
   }
+};
 
-  render(<Wrapper />);
-  //   expect(setAutoTheme).toHaveBeenCalledTimes(1);
+const store = createStore(reducer);
+
+describe('useAutoTheme behaviour', () => {
+  let windowSpy: jest.SpyInstance;
+
+  let {
+    result: { current }
+  } = renderHook(useAutoTheme, {
+    wrapper: ({ children }) => <Provider store={store}>{children}</Provider>
+  });
+
+  beforeEach(() => {
+    windowSpy = jest.spyOn(window, 'window', 'get');
+    windowSpy.mockImplementation(() => ({
+      matchMedia: jest.fn(query => MockMatchMedia(query))
+    }));
+
+    Object.defineProperty(window, 'localStorage', {
+      value: localStorageMock
+    });
+  });
+
+  afterEach(() => {
+    windowSpy.mockRestore();
+    localStorage.clear();
+  });
+
+  it('checks device mode support ', () => {
+    let modeSupports = window.matchMedia('(prefers-color-scheme)');
+    expect(modeSupports).not.toBe('not all');
+  });
+
+  it('checks dark mode support/true', () => {
+    let currentModeMatches;
+    act(() => {
+      currentModeMatches = window.matchMedia(`(prefers-color-scheme: ${THEME.dark})`).matches;
+      current.setAutoTheme();
+    });
+    let { theme } = JSON.parse(localStorage.getItem('settings')!);
+
+    expect(currentModeMatches).toBe(true);
+    expect(store.getState().settings.theme).toEqual(THEME.dark);
+    expect(theme).toBe(THEME.dark);
+  });
+
+  it('checks dark mode matches false', () => {
+    let currentModeMatches;
+    act(() => {
+      currentModeMatches = window.matchMedia(`(prefer-colors-scheme: ${THEME.light})`).matches;
+      current.setAutoTheme();
+    });
+
+    let { theme } = JSON.parse(localStorage.getItem('settings')!);
+    console.log('default', theme);
+    expect(currentModeMatches).toBeFalsy();
+  });
 });
