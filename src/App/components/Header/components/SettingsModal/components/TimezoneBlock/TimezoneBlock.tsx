a82ac7e6f33fa3aee6cf5c0b99ff1ab01237672b
Charlie Watson <charlie_watson@epam.com>
2022-08-31 14:50:53 +0100

    refactor: radio list component created

--- src/App/components/Header/components/SettingsModal/components/TimezoneBlock/TimezoneBlock.tsx
+++ src/App/components/Header/components/SettingsModal/components/TimezoneBlock/TimezoneBlock.tsx
@@ -1,46 +1,43 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 
-import { RadioGroup, Radio } from '@mui/material';
-
-import style from '../../SettingsModal.module.scss';
 import { INPUT_IDS } from '../../SettingsModal.constants';
 import { IBlocksProps } from '../../SettingsModal.types';
 import { TIMEZONE } from '../../../../../../redux/constants';
-import { truncate } from '../../../../../../utils/truncate';
+import { RadioGroup } from '@mui/material';
+import RadioList from './components/RadioList/RadioList';
+
 const TimezoneBlock: React.FC<IBlocksProps> = ({ localSettings, handleSetSettings }) => {
   const { t } = useTranslation();
-  const [isTruncate, setIsTruncate] = useState(true);
-
+  const radioGroupData = [
+    {
+      id: INPUT_IDS.disableTimezone,
+      value: TIMEZONE.disabled,
+      translate: t('Settings.DisableTimezone')
+    },
+    {
+      id: INPUT_IDS.abbreviationTimezone,
+      value: TIMEZONE.abbrv,
+      translate: t('Settings.AbbrvTimezone')
+    },
+    {
+      id: INPUT_IDS.countryTimezone,
+      value: TIMEZONE.country,
+      translate: t('Settings.CountryTimezone')
+    },
+    {
+      id: INPUT_IDS.abbreviationAndCountryTimezone,
+      value: TIMEZONE.abbrvCountry,
+      translate: t('Settings.AbbrvAndCountryTimezone')
+    }
+  ];
+  const radio = radioGroupData.map(({ id, value, translate }) => (
+    <RadioList key={id} id={id} value={value} translate={translate} />
+  ));
   return (
     <>
       <RadioGroup value={localSettings.showTimezone} onChange={handleSetSettings}>
-        <div>
-          <Radio id={INPUT_IDS.disableTimezone} value={TIMEZONE.disabled} />
-          <label className={style.cursorPointer} htmlFor={INPUT_IDS.disableTimezone}>
-            {t('Settings.DisableTimezone')}
-          </label>
-        </div>
-        <div>
-          <Radio id={INPUT_IDS.abbreviationTimezone} value={TIMEZONE.abbrv} />
-          <label className={style.cursorPointer} htmlFor={INPUT_IDS.abbreviationTimezone}>
-            {t('Settings.AbbrvTimezone')}
-          </label>
-        </div>
-        <div>
-          <Radio id={INPUT_IDS.countryTimezone} value={TIMEZONE.country} />
-          <label className={style.cursorPointer} htmlFor={INPUT_IDS.countryTimezone}>
-            {t('Settings.CountryTimezone')}
-          </label>
-        </div>
-        <div onMouseEnter={() => setIsTruncate(false)} onMouseLeave={() => setIsTruncate(true)}>
-          <Radio id={INPUT_IDS.abbreviationAndCountryTimezone} value={TIMEZONE.abbrvCountry} />
-          <label className={style.cursorPointer} htmlFor={INPUT_IDS.abbreviationAndCountryTimezone}>
-            {isTruncate
-              ? truncate(t('Settings.AbbrvAndCountryTimezone'), 24)
-              : t('Settings.AbbrvAndCountryTimezone')}
-          </label>
-        </div>
+        {radio}
       </RadioGroup>
     </>
   );
