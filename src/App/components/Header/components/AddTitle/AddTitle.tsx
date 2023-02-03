86d7bfc48c5ad603dc3e74c2a9a0f513ac36a341
moraheem98 <mojoraheem@icloud.com>
2023-02-03 11:04:31 +0000

    dashboard title component logic completed

--- src/App/components/Header/components/AddTitle/AddTitle.tsx
+++ src/App/components/Header/components/AddTitle/AddTitle.tsx
@@ -1,21 +1,29 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 export const AddTitle: React.FC = () => {
+  const inputRef = useRef(null);
   const [openModal, setOpenModal] = useState(false);
+  const [updated, setUpdated] = useState('');
   const { t } = useTranslation();
+
+  const handleClickFunction = () => {
+    setUpdated(inputRef.current.value);
+    setOpenModal(false);
+  };
 
   return (
     <>
       {openModal ? (
         <>
-          <input placeholder="Type Here" />
-          <button onClick={() => console.log('hello')}>{t('AddTitle.Submit')}</button>
+          <input placeholder="Type Here" ref={inputRef} />
+          <button onClick={() => handleClickFunction()}>{t('AddTitle.Submit')}</button>
           <button onClick={() => setOpenModal(false)}>{t('AddTitle.Close')}</button>
         </>
       ) : (
         <button onClick={() => setOpenModal(true)}>{t('AddTitle.Edit')}</button>
       )}
+      <h1>{updated}</h1>
     </>
   );
 };
